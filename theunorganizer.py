d41d9edd6bd727468876f38b294fa3d7520d218a
jprmesh <mrachhh@gmail.com>
2016-01-22 23:06:35 -0500

    Serve a page yay. It gets location or something.

--- theunorganizer.py
+++ theunorganizer.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/location")
+def getlocation():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
